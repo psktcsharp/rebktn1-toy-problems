1884b4924cc0b953c243e458f81c3d406067bcfb
unknown <safeerlibya@gmail.com>
2019-11-26 09:47:36 +0100

    my attempt

--- blackjackScorer/blackjack.js
+++ blackjackScorer/blackjack.js
@@ -13,3 +13,42 @@ Examples
 ["A", "10", "A"]                ==>  12
 ["5", "3", "7"]                 ==>  15
 ["5", "4", "3", "2", "A", "K"]  ==>  25 */
+
+var Blackjack = function(array){
+	//assuming that we will recive an array with the cards for the hand of the player
+	//creat an array to hold all integer values to sum them up at the end
+	var toSumArray = [];
+	//we loop through the array and check if it's a number between 2-10
+for (var i = 0 ; i < array.length; i++) {
+	if(Number.isInteger(array[i])){
+			if(array[i] > 2 && array[i] < 10){
+			// if its int and between the numbers we push it as is
+			toSumArray.push(array[i])
+			}
+	}
+			//we check if its jack or qeen or king to push 10
+			if(array[i] === "J" || array[i] === "Q" ||  array[i] === "K"  ){
+				toSumArray.push(10);
+			}
+			//if not it must be Ace, before we count ace as 11 we must make sure 
+			//the total sum we have didn't excced 21 or we push it as 1 instead
+			var currentSum = 0;
+			for (var x = 0 ; x < toSumArray.length; x++) {
+				if(Number.isInteger(toSumArray[x])){
+				currentSum = currentSum + parseInt(toSumArray[x]);
+			}
+			}
+			var checkTotal = 21 - currentSum;
+			if(checkTotal <= 10){
+				toSumArray.push(11);
+			}
+			toSumArray.push(1);
+}
+//at the end we return the total of the hand based on the numbers we have in the sumrray
+var result = 0;
+for (var i = 0; i < toSumArray.length; i++) {
+	result = result + parseInt(toSumArray[i]);
+}
+return result;
+
+};
