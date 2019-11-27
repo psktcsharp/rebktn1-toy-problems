4d506a5c899fef66bbee33a299863169ed6db1bb
unknown <safeerlibya@gmail.com>
2019-11-27 09:40:10 +0100

    my attempt

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,27 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  //create a holder for the strings
+  var mem ={};
+  //append chars from string 1 to it
+  for (var i = 0; i < string1.length; i++) {
+  	mem[string1[i]] = 1;
+  	//console.log(mem);
+  }
+  //append chars from string 2 to it 
+  for (var i = 0; i < string2.length; i++) {
+  	//check if key already there make it ++
+  	if(mem[string2[i]]){
+  		mem[string2[i]]++;
+  		console.log(mem);
+  	}
+  	//else ignore it
+  }
+  //loop and return all keys bigger than 1
+  	return mem;
+// 	Object.entries(mem).forEach(([key, value]) => {
+// 		if(value > 1)
+// 		 return key;
+// });
+
 };
