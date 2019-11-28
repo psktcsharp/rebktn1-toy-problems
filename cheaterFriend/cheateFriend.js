821dbcd2c6962717d6062f56377c19980aa5b724
unknown <safeerlibya@gmail.com>
2019-11-28 09:56:01 +0100

    my answer

--- cheaterFriend/cheateFriend.js
+++ cheaterFriend/cheateFriend.js
@@ -17,3 +17,35 @@
 //
 // Example:
 // removNb(26) should return [ {15, 21}, {21, 15} ]
+
+var removNb = function(n){
+	//create an array of numbers from 1 to the n passed in the function
+	var numbers = [];
+	for (var i = 1; i <= n; i++) {
+		numbers.push(i);
+	}
+	//calculate sum of the array
+	var sum = 0;
+	for (var i = 0; i < numbers.length; i++) {
+		sum = sum + numbers[i];
+	}
+	//pick 2 numbers and test if a*b - (a+b) = sum
+	for (var i = 0; i < numbers.length; i++) {
+		for (var j = 0; j < numbers.length; j++) {
+		var a = numbers[i];
+		var b = numbers[j];
+		var product = a*b;
+		
+		//console.log('product is : ' + product);
+		var abSum = a+b;
+		//console.log('absum is : ' + abSum);
+		var exclud = product - abSum;
+		//console.log('exlud is : ' + exclud);
+		//console.log('this is sum : ' + sum)
+		if(exclud === sum){		
+			return [a,b];
+		}
+		}
+	}
+	return "not found";
+}
