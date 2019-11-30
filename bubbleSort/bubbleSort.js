1366e88cf0172a833006fb7745338bfb16d1804c
unknown <safeerlibya@gmail.com>
2019-11-30 09:22:22 +0100

    bubble attempt

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -35,4 +35,13 @@
 
 var bubbleSort = function(array) {
   // Your code here.
+  for (var i = 0; i < array.length; i++) {
+  		if(array[i] > array[i+1])
+  		{
+  			var temp = array[i];
+  			array[i] = array[i+1];
+  			array[i+1] = temp;
+  		}
+  }
+  return array;
 };
