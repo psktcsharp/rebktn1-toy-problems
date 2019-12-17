1e0371adee935da25cd1f4a138eec02a4e2758c9
psktcsharp <safeerlibya@gmail.com>
2019-12-17 09:20:14 +0100

    attempt

---
+++ composePipe/composePipe.js
@@ -0,0 +1,54 @@
+/*
+ * Write Compose and Pipe functions.
+ * 
+ * Step 1: Implement the function Compose: 
+ *
+ * Compose should return a function that is the composition of a list of
+ * functions of arbitrary length.
+ *
+ * Each function is called on the return value of the function that follows.
+ *
+ * You can view compose as moving right to left through its arguments.
+ *
+ * Compose Example:
+ *   var greet = function(name){ return 'hi: ' + name;}
+ *   var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+ *   var welcome = compose(greet, exclaim);
+ *   welcome('phillip'); // 'hi: PHILLIP!'
+ *
+ * Step 2: Implement the function Pipe:
+ *
+ * Pipe composes a series of functions and returns the resulting function.
+ * 
+ * Each function is called on the return value of the preceding function.
+ *
+ * You can view pipe as moving left to right through its arguments.
+ * 
+ * Pipe Example:
+ *  var add2 = function(number){ return number + 2; }
+ *  var multiplyBy3 = function(number){ return number * 3; }
+ *  pipe(add2, multiplyBy3)(5) // 21
+ *  pipe(add2, multiplyBy3, multiplyBy3)(5) // 63
+ */
+
+'use strict';
+var greet = function(name){ return 'hi: ' + name;}
+var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+var compose = function(fn1,fn2) {
+    fn2(fn1())
+  return fn1 
+};
+
+
+var pipe = function() {
+    // fn1(fn2())
+    // return fn1 
+
+    //     for(i=0; i< arguments;i++){
+//     var fn = arguments[i]
+//     var fn2 = arguments[i+1]
+//     fn(fn2())
+//     return fn1
+//     }
+  
+};
